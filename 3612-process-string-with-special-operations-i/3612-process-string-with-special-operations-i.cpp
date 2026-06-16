3873eb432e2e3cfab765e4bdde152c7220148bb5
tripati291 <91694051+tripati291@users.noreply.github.com>
2026-06-16 12:04:48 +0530

    Time: 1 ms (83.71%), Space: 39.7 MB (87.5%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result;
+        for(int i=0; i<s.size(); i++) {
+            if(s[i]>='a' && s[i]<= 'z') {
+                result += s[i];
+            }
+            else if(s[i]=='*') {
+                if(!result.empty()) {
+                    result.pop_back();
+                }
+            }
+            else if(s[i]=='#') {
+                result.append(result);
+            }
+            else if(s[i]=='%') {
+                reverse(result.begin(), result.end());
+            }
+        }
+        return result;
+    }
+};
