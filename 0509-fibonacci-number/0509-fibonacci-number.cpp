860cb0adc0d1174616070985d75d7511ac3b5158
tripati291 <91694051+tripati291@users.noreply.github.com>
2026-06-21 15:36:13 +0530

    Time: 18 ms (22.48%), Space: 8 MB (19.92%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int f(int i, vector<int>& dp) {
+        if(i==0) return 0;
+        if(i==1) return 1;
+        if(dp[i]!=-1) return dp[i];
+        return f(i-1,dp) + f(i-2, dp);
+    }
+    int fib(int n) {
+        vector<int> dp(n+1, -1);
+        
+        return f(n, dp);
+    }
+};
