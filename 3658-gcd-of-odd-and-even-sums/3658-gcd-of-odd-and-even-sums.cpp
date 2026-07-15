47e0345d08020ef837821b714acede4b5cba886b
tripati291 <91694051+tripati291@users.noreply.github.com>
2026-07-15 11:44:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        if(n==1) return 1;
-        int x = ((2*n)*(2*n + 1))/2;
-        int odd = x/2 - n/2 ;
-        int even = x - odd;
-        int ans=0;
-        int maxi=0;
-        for(int i=1; i<= odd/2 ; i++) {
-            if(odd%i ==0 && even%i ==0) {
-                 maxi = i;
-            }
-            ans = max(ans , maxi);
-        }
-        return ans;
+        return n;
     }
 };
